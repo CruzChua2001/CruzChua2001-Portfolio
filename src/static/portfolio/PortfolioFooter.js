6710e7d891649d9ecb6125b78e8095223d58f9e8
CruzChua2001 <89196728+CruzChua2001@users.noreply.github.com>
2023-05-25 03:39:09 +0800

    Fix: Update hard-coded text from Footer to appsettings.json

--- src/static/portfolio/PortfolioFooter.js
+++ src/static/portfolio/PortfolioFooter.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 
+import jsonConfig from '../../../appsettings.json';
+
 const PortfolioFooter = _ => {
   return (
     <MDBFooter bgColor='dark' className='w-100 pt-3 text-center text-lg-start text-light border-top' style={{height: "40%"}}>
@@ -10,18 +12,18 @@ const PortfolioFooter = _ => {
             <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>
                 <MDBIcon icon="gem" className="me-3" />
-                Cruz Chua
+                {jsonConfig.CONFIG.FULL_NAME}
               </h6>
               <p>
-                I aspire to be a Software Developer and to continue to improve as one. I appreciate you taking the time to visit my portfolio.
+                {jsonConfig.FOOTER.DESCRIPTION}
               </p>
             </MDBCol>
 
             <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Socials</h6>
-              <p> <a href='https://www.linkedin.com/in/cruz-c-69a027130/' target="_blank" className='text-reset'>LinkedIn</a> </p>
-              <p> <a href='https://github.com/CruzChua2001' target="_blank" className='text-reset'>GitHub</a> </p>
-              <p> <a href='https://www.instagram.com/justcruzc/' target="_blank" className='text-reset'>Instagram</a> </p>
+              <p> <a href={jsonConfig.FOOTER.LINKEDIN_URL} target="_blank" className='text-reset'>LinkedIn</a> </p>
+              <p> <a href={jsonConfig.FOOTER.GITHUB_URL} target="_blank" className='text-reset'>GitHub</a> </p>
+              <p> <a href={jsonConfig.FOOTER.INSTAGRAM_URL} target="_blank" className='text-reset'>Instagram</a> </p>
             </MDBCol>
 
             <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
@@ -34,9 +36,9 @@ const PortfolioFooter = _ => {
 
             <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p> <MDBIcon icon="home" className="me-2" /> 795, Yishun Ring Road, S760795 </p>
-              <p> <MDBIcon icon="envelope" className="me-3" /> <a href="mailto:2001cruzchua@gmail.com">2001cruzchua@gmail.com</a> </p>
-              <p> <MDBIcon icon="phone" className="me-3" /> <a href="tel:+6592773372">+65 9277 3372</a> </p>
+              <p> <MDBIcon icon="home" className="me-2" /> {jsonConfig.CONFIG.ADDRESS} </p>
+              <p> <MDBIcon icon="envelope" className="me-3" /> <a href={"mailto:"+jsonConfig.CONFIG.EMAIL}>{jsonConfig.CONFIG.EMAIL}</a> </p>
+              <p> <MDBIcon icon="phone" className="me-3" /> <a href={"tel:"+jsonConfig.CONFIG.PHONE}>{jsonConfig.CONFIG.PHONE}</a> </p>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
